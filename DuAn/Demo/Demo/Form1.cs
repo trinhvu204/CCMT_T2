d94465c50c3c8037477697b0ac9d3a96c557c357
TrungKien13 <133593440+TrungKien13@users.noreply.github.com>
2025-09-15 10:28:14 +0700

    Revert "Login"

--- DuAn/Demo/Demo/Form1.cs
+++ DuAn/Demo/Demo/Form1.cs
@@ -16,5 +16,18 @@ namespace Demo
         {
             InitializeComponent();
         }
+<<<<<<< HEAD
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+=======
+>>>>>>> 3491b5a0bf82f6406dcdbb2c7206e307ff40397f
     }
 }
